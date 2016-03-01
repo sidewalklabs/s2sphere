4cd71e2208af2d1f6000b7667cb1c17d7a644df5
Alexander Mohr <amohr@farmersbusinessnetwork.com>
2016-02-29 16:49:48 -0800

    bugfix

--- setup.py
+++ setup.py
@@ -4,14 +4,9 @@ import os
 from setuptools import setup, find_packages
 
 
-def read(f):
-    return open(os.path.join(os.path.dirname(__file__), f)).read().strip()
-
-
 setup(name='s2-sphere',
-      version='0.1.0',
+      version='0.1.1',
       description='Python implementation of the amazing S2 Geometry Library',
-      long_description=read('README.md'),
       author='Jonathan Gillham',
       author_email='',
       url='http://github.com/qedus/sphere',
