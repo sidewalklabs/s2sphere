e057cae367d135aefa300bb65ead672958470fce
Sven Kreiss <me@svenkreiss.com>
2016-04-19 09:36:07 -0400

    update test

--- tests/covering_test.py
+++ tests/covering_test.py
@@ -6,19 +6,19 @@ class CoveringTest(unittest.TestCase):
     def test_covering(self):
         r = s2sphere.RegionCoverer()
         p1 = s2sphere.LatLon.from_degrees(33, -122)
-        p2 = s2sphere.LatLon.from_degrees(33, -122.1)
+        p2 = s2sphere.LatLon.from_degrees(33.1, -122.1)
         cell_ids = r.get_covering(s2sphere.LatLonRect.from_point_pair(p1, p2))
         ids = sorted([c.id() for c in cell_ids])
-        target = [9291046973249421312,
-                  9291046994724257792,
-                  9291047097803472896,
-                  9291051444310376448,
-                  9291051856627236864,
-                  9291051994066190336,
-                  9291052195929653248,
-                  9291052268944097280]
-        print(ids)
-        print(target)
+        target = [9291041754864156672,
+                  9291043953887412224,
+                  9291044503643226112,
+                  9291045878032760832,
+                  9291047252422295552,
+                  9291047802178109440,
+                  9291051650468806656,
+                  9291052200224620544]
+        print('  result: {}'.format(ids))
+        print('expected: {}'.format(target))
         assert ids == target
 
 
