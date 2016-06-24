005fa36cc7e2788d7d7435a317989f887e1282ce
Sven Kreiss <me@svenkreiss.com>
2016-06-24 11:25:52 -0400

    add Sphinx extension for source links (#21)

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -42,6 +42,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
